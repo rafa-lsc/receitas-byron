ccf8bcc7a0ecdd89ebea5eee8980148b1e45475e
Rafael <rafa.scarpel@gmail.com>
2025-06-29 15:14:33 -0300

    feat: estrutura da seção Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex-grow">
-      <h1>Tela Inicial</h1>
+      <div className="container mx-auto">
+        {/* Seção hero */}
+
+        <section>
+          <h1>Receitas Deliciosas</h1>
+          <p>Descubra receitas simples e saborosas para todas as ocasiões</p>
+
+          <Link href="/receitas">
+            Ver todas as receitas 
+          </Link>
+        </section>
+
+      </div>
     </main>
   );
 }
