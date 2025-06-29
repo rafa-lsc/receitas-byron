30ac1fa437001c10b98cda9ed8ea40a8c5eb2d5a
Rafael Scarpel <167385916+rafa-lsc@users.noreply.github.com>
2025-06-29 16:22:01 -0300

    Revert "Feature: home page"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
@@ -14,21 +13,6 @@ export default function Home() {
           <Link className="bg-blue-500 hover:bg-blue-700 transition-colors text-white font-bold rounded-lg px-3 py-2" href="/receitas">
             Ver todas as receitas
           </Link>
-        </div>
-      </section>
-
-      {/* seção receitas em destaque */}
-      <section className="py-12">
-        <div className="flex flex-col items-center container mx-auto">
-          <h2 className="text-lg font-bold">Receitas em destaque</h2>
-
-          {/* cards de receita */}
-
-          <Link className="flex text-blue-400 hover:text-blue-700 transition-colors" href="/receitas">
-            Ver todas as receitas
-            <ChevronRight />
-          </Link>
-
         </div>
       </section>
     </main>
