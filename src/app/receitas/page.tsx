1f188ceb577b5dcbfabf368cb13d121a2cf965ae
Rafael <rafa.scarpel@gmail.com>
2025-07-13 14:04:03 -0300

    feat: setup do axios e requisicao para todas as receitas

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -1,21 +1,35 @@
 "use client";
 
 import RecipeCard from "@/components/RecipeCard";
-import { recipes as initialRecipes } from "../../lib/data";
 import type { Recipe } from "@/lib/data";
 import { Plus } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RecipeFormModal from "@/components/RecipeFormModal";
 import DeleteConfirmationModal from "@/components/DeleteConfirmationModal";
+import api from "@/lib/api";
 
 export default function ReceitasPage() {
   const [isRecipeModalOpen, setIsRecipeModalOpen] = useState(false);
   const [isDeleteConfirmationModalOpen, setIsDeleteConfirmationModalOpen] = useState(false)
-  const [recipes, setRecipes] = useState<Recipe[]>(initialRecipes);
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [modalMode, setModalMode] = useState<"create" | "edit">("create");
   const [selectedRecipe, setSelectedRecipe] = useState<Recipe | undefined>(
     undefined
   );
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await api.get("/recipes");
+
+        setRecipes(response.data);
+      } catch (error) {
+        console.error("Erro ao requisitar as receitas", error);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
 
   const handleOpenCreateModal = () => {
     setModalMode("create");
